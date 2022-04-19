f917551db21f174e157dd2bbf0338ccafed77773
Marx Costanzo Jr <marxcostanzo@yahoo.com>
2022-04-19 01:27:55 -0400

    Added user authorized

--- routes/connectionRoutes.js
+++ routes/connectionRoutes.js
@@ -1,32 +1,28 @@
 const express = require('express');
 const controller = require('../controllers/connectionController');
+const { isLoggedIn, isHost } = require('../middlewares/auth');
+
 const router = express.Router();
 
 //GET /connections: view all connections 
-
 router.get('/', controller.index);
 
 //GET /connections/new: send html form for creating a new connection
-
-router.get('/new', controller.new);
+router.get('/new', isLoggedIn, controller.new);
 
 //POST /stories: create a new connection
-
-router.post('/', controller.create);
+router.post('/', isLoggedIn, controller.create);
 
 //GET /connections/:id: send details of connection identified by id
-
 router.get('/:id', controller.show);
 
 //GET /connections/:id/edit: send html form for editing an existing connection
-
-router.get('/:id/edit', controller.edit);
+router.get('/:id/edit', isLoggedIn, isHost, controller.edit);
 
 //PUT /connections/:id: update the connection identified by id
-
-router.put('/:id', controller.update);
+router.put('/:id', isLoggedIn, isHost, controller.update);
 
 //DELETE /connections/:id, delete the connection identified by id
-router.delete('/:id', controller.delete);
+router.delete('/:id', isLoggedIn, isHost, controller.delete);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const controller = require('../controllers/userController');
-
+const { isGuest, isLoggedIn } = require('../middlewares/auth');
 
 const router = express.Router();
 
 //GET /users/new: send html form for creating a new user account
-router.get('/new', controller.new);
+router.get('/new', isGuest, controller.new);
 
 //POST /users: create a new user account
-router.post('/', controller.create);
+router.post('/', isGuest, controller.create);
 
 //GET /users/login: send html for logging in
-router.get('/login', controller.getUserLogin);
+router.get('/login', isGuest, controller.getUserLogin);
 
 //POST /users/login: authenticate user's login
-router.post('/login', controller.login);
+router.post('/login', isGuest, controller.login);
 //GET /users/profile: send user's profile page
-router.get('/profile', controller.profile);
+router.get('/profile', isLoggedIn, controller.profile);
 
 //POST /users/logout: logout a user
-router.get('/logout', controller.logout);
+router.get('/logout', isLoggedIn, controller.logout);
 
 module.exports = router;
