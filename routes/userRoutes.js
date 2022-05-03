b56cee826db5a2d34faf5c75d89091a7103e7feb
Marx Costanzo Jr <marxcostanzo@yahoo.com>
2022-05-03 18:08:11 -0400

    Added a rate limiter for login requests

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const controller = require('../controllers/userController');
 const { isGuest, isLoggedIn } = require('../middlewares/auth');
+const { logInLimiter } = require('../middlewares/rateLimiters');
 
 const router = express.Router();
 
@@ -14,7 +15,7 @@ router.post('/', isGuest, controller.create);
 router.get('/login', isGuest, controller.getUserLogin);
 
 //POST /users/login: authenticate user's login
-router.post('/login', isGuest, controller.login);
+router.post('/login', logInLimiter, isGuest, controller.login);
 //GET /users/profile: send user's profile page
 router.get('/profile', isLoggedIn, controller.profile);
 
